a819d7b6aaedf5dd1d33f7b339b92f7f451ab57f
Mouad <69320211+ZagreusIV@users.noreply.github.com>
2023-12-10 16:22:07 +0100

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,18 +12,6 @@
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 int	ft_set_char(char c, char const *str)
 {
 	size_t	i;
