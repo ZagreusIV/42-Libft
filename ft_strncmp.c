6cc169098753a5ec21b4df27a3f869ea1e684080
Mouad <69320211+ZagreusIV@users.noreply.github.com>
2023-12-10 16:12:23 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,29 +12,6 @@
 
 #include "libft.h"
 
-/*int	charcmp(char s1, char s2)
-{
-	if ((unsigned char)s1 != (unsigned char)s2)
-		return ((unsigned char)s1 - (unsigned char)s2);
-	return (0);
-}
-
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	while (s2[i] && s1[i] && i < n)
-	{
-		if (charcmp(s1[i], s2[i]))
-			return (s1[i] - s2[i]);
-		i++;
-	}
-	if (i < n)
-		return (charcmp(s1[i], s2[i]));
-	return (0);
-}*/
-
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
