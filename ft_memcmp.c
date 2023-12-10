b59dada586e2000e3637307735d7eea0837548c6
Mouad <69320211+ZagreusIV@users.noreply.github.com>
2023-12-10 16:06:06 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
 #include "libft.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
@@ -26,20 +25,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
-
-/*int main() {
-    char *str1 = NULL;
-    char *str2 = NULL;
-
-    int result = ft_memcmp(str1, str2, 5);
-
-    if (result == 0) {
-        printf("The two strings are equal.\n");
-    } else if (result < 0) {
-        printf("str1 is less than str2.\n");
-    } else {
-        printf("str1 is greater than str2.\n");
-    }
-
-    return 0;
-}*/
